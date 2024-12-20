50d78efe43743fed314bc8796d8ba58d3a4d8990
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-12-20 17:21:19 +0000

    Added auth, cache and errorhandling middleware: all cleaned up

---
+++ src/middleware/cache.js
@@ -0,0 +1,29 @@
+const { redisClient } = require("../config/redis");
+const logger = require("../config/logger");
+
+const cache = (duration) => {
+  return async (req, res, next) => {
+    const key = `cache:${req.originalUrl}`;
+
+    try {
+      const cachedData = await redisClient.get(key);
+
+      if (cachedData) {
+        return res.json(JSON.parse(cachedData));
+      }
+
+      res.originalJson = res.json;
+      res.json = async (data) => {
+        await redisClient.setEx(key, duration, JSON.stringify(data));
+        res.originalJson(data);
+      };
+
+      next();
+    } catch (error) {
+      logger.error("Cache middleware error:", error);
+      next();
+    }
+  };
+};
+
+module.exports = cache;
