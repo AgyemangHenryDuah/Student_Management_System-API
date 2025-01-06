c231a50d6ad283c067b63a5f6fc52aaea6f36a99
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-01-06 10:50:56 +0000

    Added jest configuration file and setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    testTimeout: 50000,
-    testEnvironment: "node", // Ensures a Node.js environment for testing.
-    testMatch: ["**/__tests__/**/*.test.js"], // Match files ending with `.test.js`.
-    collectCoverage: true, // Enables code coverage collection.
-    coverageDirectory: "coverage", // Directory for coverage reports.
-    moduleDirectories: ["node_modules", "src"], // Adjust based on your app structure.
+    testEnvironment: "node",
+    testMatch: ["**/src/__tests__/**/*.test.js"],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    moduleDirectories: ["node_modules", "src"],
+    setupFiles: ["<rootDir>/src/__tests__/utils/setup.js"],
+    coveragePathIgnorePatterns: ["/node_modules/",
+        "src/models/",
+        "src/routes/",
+        "src/utils/",
+        "src/config/",
+        "src/validators/",
+        "src/swagger/",
+    "src/middleware/"],
 };
