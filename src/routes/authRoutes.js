88e922d7330ab766859c56fa4845962ce699d2fa
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-01-09 20:04:57 +0000

    Added new version of auth routes to include reset password and refresh token routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { auth, authorize } = require("../middleware/auth");
 
 /**
  * @swagger
@@ -43,6 +44,39 @@ router.post("/login", authController.login);
 
 /**
  * @swagger
+ * /api/auth/refresh-token:
+ *   post:
+ *     tags: [Authentication]
+ *     summary: Refresh token
+ *     description: Generate a new access token using a refresh token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 format: token
+ *     responses:
+ *       200:
+ *         description: Access token generated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RefreshTokenResponse'
+ *       400:
+ *         description: Refresh token is required
+ *       500:
+ *         description: Invalid or expired refresh token
+ */
+router.post("/refresh-token", authController.refreshToken);
+
+/**
+ * @swagger
  * /api/auth/reset-password:
  *   post:
  *     tags: [Authentication]
@@ -65,7 +99,9 @@ router.post("/login", authController.login);
  *         description: Password reset email sent
  *       404:
  *         description: User not found
+ *       500:
+ *         description: Error sending email
  */
-router.post("/reset-password", authController.resetPassword);
+router.post("/reset-password",authController.resetPassword);
 
 module.exports = router;
