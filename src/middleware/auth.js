ae2f23883a464ec87a672d22c1b2944ab2e0fd68
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-01-09 20:03:51 +0000

    Added new version of auth middlware with own profile function created for student update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -32,41 +32,22 @@ const authorize = (...roles) => {
   };
 };
 
-module.exports = { auth, authorize };
 
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/User");
+const OwnProfile = (req, res, next) => {
+  const studentId = req.params.id;
+  if (req.user.role === 'instructor') {
+    return next();
+  }
 
-// const auth = async (req, res, next) => {
-//   try {
-//     const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (req.user._id.toString() !== studentId.toString()) {
+    return res.status(403).json({
+      message: "You can only update your own profile"
+    });
+  }
+  next();
+};
 
-//     if (!token) {
-//       return res.status(401).json({ message: "Authentication required" });
-//     }
+module.exports = OwnProfile;
 
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findById(decoded.id);
 
-//     if (!user) {
-//       return res.status(401).json({ message: "User not found" });
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: "Invalid token" });
-//   }
-// };
-
-// const authorize = (...roles) => {
-//   return (req, res, next) => {
-    
-//     if (!roles.includes(req.user.role)) {
-//       return res.status(403).json({ message: "Access denied" });
-//     }
-//     next();
-//   };
-// };
-
-// module.exports = { auth, authorize };
+module.exports = { auth, authorize, OwnProfile };
